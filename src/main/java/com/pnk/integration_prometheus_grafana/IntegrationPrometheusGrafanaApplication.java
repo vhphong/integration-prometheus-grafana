a23fef9266bc91238cd16e60b8e930c69296a99e
vhphong <vhphong@gmail.com>
2024-12-30 21:45:33 -0600

    Add existing project files to Git

---
+++ src/main/java/com/pnk/integration_prometheus_grafana/IntegrationPrometheusGrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.pnk.integration_prometheus_grafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationPrometheusGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationPrometheusGrafanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pnk/integration_prometheus_grafana/IntegrationPrometheusGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnk.integration_prometheus_grafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationPrometheusGrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
